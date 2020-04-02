7dc5de18165a68c078a12e0913642ae0df5d2c76
ilkerispir <ikerispir@gmail.com>
2020-04-03 01:13:24 +0300

    connect server side and dialogflow

---
+++ chatbot/chatbot.js
@@ -0,0 +1,55 @@
+const dialogflow = require('dialogflow');
+const structjson = require('./structjson.js');
+const config = require('../config/keys');
+
+const projectID = config.googleProjectID;
+const credentials = {
+    client_email: config.client_email,
+    private_key: config.private_key
+}
+
+const sessionClient = new dialogflow.SessionsClient({projectID, credentials});
+const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
+
+
+module.exports = {
+    textQuery: async function(text, parameters = {}) {
+        let self = module.exports;
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    text: text,
+                    languageCode: config.dialogFlowSessionLanguageCode,
+                },
+            },
+            queryParams: {
+                payload: {
+                    data: parameters
+                }
+            }
+        };
+        let responses = await sessionClient.detectIntent(request);
+        responses = await self.handleAction(responses);
+        return responses;
+    },
+    eventQuery: async function(event, parameters = {}) {
+        let self = module.exports;
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                event: {
+                    name: event,
+                    parameters: structjson.jsonToStructProto(parameters),
+                    languageCode: config.dialogFlowSessionLanguageCode,
+                },
+            }
+        };
+        let responses = await sessionClient.detectIntent(request);
+        responses = await self.handleAction(responses);
+        return responses;
+    }, 
+    handleAction: function(responses){
+        return responses;
+    }
+} 
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send({'hello': 'world!'});
-});
+require('./routes/dialogflow')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
