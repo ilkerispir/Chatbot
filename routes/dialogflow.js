668209e627e53ffec092efa1ff5c239cab0729ad
ilkerispir <ikerispir@gmail.com>
2020-04-03 01:21:09 +0300

    connect server side and dialogflow

--- routes/dialogflow.js
+++ routes/dialogflow.js
@@ -3,7 +3,7 @@ const chatbot = require('../chatbot/chatbot');
 module.exports = app => {
 
     app.get('/', (req, res) => {
-        res.send({'hello': 'world'})
+        res.send({'hello': 'Johnny'});
     });
 
     app.post('/api/df_text_query', async (req, res) => {
